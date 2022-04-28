0f0eb9efdb451e46ddbdd7dbb0f6a06a2a004b4b
RohitD007 <dsouza.rohit.94@gmail.com>
2022-04-27 23:15:35 -0400

    Initial commit

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,17 +1,19 @@
 # project/tests/test_config.py
+"""this test config"""
 import logging
 import os
 
 import app.config
 
-
 def test_development_config(application):
+    """this test dev config"""
     application.config.from_object('app.config.DevelopmentConfig')
 
     assert application.config['DEBUG']
     assert not application.config['TESTING']
 
 def test_testing_config(application):
+    """this test testing config"""
     application.config.from_object('app.config.TestingConfig')
     assert application.config['DEBUG']
     assert application.config['TESTING']
@@ -19,6 +21,7 @@ def test_testing_config(application):
     assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///:memory:'
 
 def test_production_config(application):
+    """this test prod config"""
     application.config.from_object('app.config.ProductionConfig')
     assert not application.config['DEBUG']
     assert not application.config['TESTING']
